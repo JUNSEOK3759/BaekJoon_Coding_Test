c9c3524b613e5527519caa8518d377c72c637c4c
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-01-30 15:50:25 +0900

    [Silver I] Title: 안전 영역, Time: 828 ms, Memory: 34148 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,30 +1,41 @@
 import sys
 from collections import deque
-n = int(input())
+# sys.stdin = open("input.txt", "rt")
 
-a = [list(map(int, input().split())) for _ in range(n)]
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-mini = min(map(min, a))
-maxi = max(map(max, a))
-res = 0
-dq = deque()
+def bfs(x, y, h):
+    dq = deque()
+    dq.append([x, y])
+
+    while dq:
+        x, y = dq.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx > -1 and ny > -1 and nx < n and ny < n:
+                if ch[nx][ny] == 0 and board[nx][ny] > h:
+                    ch[nx][ny] = 1
+                    dq.append([nx, ny])
+
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+res = -2147483647
+
+maxi = max(map(max, board))
+mini = min(map(min, board))
+
 for h in range(mini-1, maxi):
     ch = [[0 for _ in range(n)] for _ in range(n)]
     cnt = 0
     for i in range(n):
         for j in range(n):
-            if a[i][j] > h and ch[i][j] == 0:
-                ch[i][j] = 1
+            if board[i][j] > h and ch[i][j] == 0:
                 cnt += 1
-                dq.append([i, j])
-                while dq:
-                    x, y = dq.popleft()
-                    for k in range(4):
-                        nx = x + dx[k]
-                        ny = y + dy[k]
-                        if nx > -1 and ny > -1 and nx < n and ny < n and a[nx][ny] > h and ch[nx][ny] == 0:
-                            ch[nx][ny] = 1
-                            dq.append([nx, ny])
-    res = max(res, cnt)
+                ch[i][j] = 1
+                bfs(i, j, h)
+    if cnt > res:
+        res = cnt
+          
 print(res)
