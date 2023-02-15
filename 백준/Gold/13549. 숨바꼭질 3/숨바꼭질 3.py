086cbc36453be006206227d206484f26ac1dc1ee
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-02-16 00:02:46 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 116 ms, Memory: 39508 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -8,28 +8,30 @@ import itertools
 input = sys.stdin.readline
 # readline을 문자열에서 쓸 때는 rstrip()를 붙여줘라
 sys.setrecursionlimit(10**9)
+# 가장 일찍 끝나는 회의를 먼저 하도록 정렬 해준다
 
 def bfs(x):
     dq = deque()
     dq.append(x)
-    ch = [0] *100001
+    ch = [0 for _ in range(100001)]
     while dq:
         x = dq.popleft()
         if x == k:
             return ch[x]
         for next in (x-1, x+1, x*2):
-            if next >= 0 and next < 100001:
+            if next >=0 and next < 100001:
                 if ch[next] == 0:
                     if next == x*2 and next != 0:
-                        ch[next] = ch[x]
                         dq.appendleft(next)
+                        ch[next] = ch[x]
                     else:
-                        ch[next] = ch[x] + 1
                         dq.append(next)
+                        ch[next] = ch[x] + 1
     
-
+                    
 
 n, k = map(int, input().split())
+
 
 
 print(bfs(n))
