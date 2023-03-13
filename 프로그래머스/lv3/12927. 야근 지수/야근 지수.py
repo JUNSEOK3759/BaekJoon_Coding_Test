994ef00860e18e5b55de8e0304bca57b871d7b86
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-03-13 20:58:29 +0900

    [level 3] Title: 야근 지수, Time: 402.97 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.py
@@ -0,0 +1,13 @@
+import heapq
+def solution(n, works):
+    if n > sum(works):
+        return 0
+    works = [-i for i in works]
+    heapq.heapify(works)
+    print(works)
+    for _ in range(n):
+        x = heapq.heappop(works)
+        x += 1
+        heapq.heappush(works, x)
+    
+    return sum([i**2 for i in works])
