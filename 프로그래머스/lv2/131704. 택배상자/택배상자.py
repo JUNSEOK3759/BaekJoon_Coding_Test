a8463c2928fd70b0c87263ee160f73479ed95166
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-03-13 19:13:38 +0900

    [level 2] Title: 택배상자, Time: 486.34 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.py
@@ -0,0 +1,14 @@
+def solution(order):
+    temp = []
+    i = 1
+    now = 0
+    while i != len(order)+1:
+        temp.append(i)
+        while temp[-1] == order[now]:
+            now += 1
+            temp.pop()
+            
+            if len(temp) == 0:
+                break
+        i += 1
+    return now
