f3d5e0bb0b3baaefaf23cc08db57686c194c983b
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-03-22 21:36:34 +0900

    [level 3] Title: 정수 삼각형, Time: 36.30 ms, Memory: 14.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,11 +1,7 @@
-from collections import deque
 def solution(t):
     for i in range(1, len(t)):
-        for j in range(i+1):
-            if j == 0:
-                t[i][j] += t[i-1][j]
-            elif j == i:
-                t[i][j] += t[i-1][j-1]
-            else:
-                t[i][j] += max(t[i-1][j], t[i-1][j-1])
-    return max(map(max, t))
+        t[i][0] += t[i-1][0]
+        t[i][i] += t[i-1][i-1]
+        for j in range(1, i):
+            t[i][j] += max(t[i-1][j], t[i-1][j-1])
+    return max(t[-1])
