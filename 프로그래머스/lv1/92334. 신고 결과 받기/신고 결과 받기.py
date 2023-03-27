4e2a49911983f99bd7a080ff15778715248fe58c
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-03-27 20:31:25 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,36 +1,16 @@
-# def solution(id_list, report, k):
-    # answer = [0] * len(id_list)
-    # a = {}
-    # for i in set(report):
-    #     x = i.split()
-    #     if x[1] in a.keys():
-    #         a[x[1]].add(x[0])
-    #     else:
-    #         a[x[1]] = {x[0]}
-    # print(a)
-    # lis = list()
-    # for values in a.values():
-    #     if len(values) >= k:
-    #         lis.append(list(values))
-    #         # for i in values:
-    #         #     answer[id_list.index(i)] += 1
-    # print(lis)
-    # return answer
-
 def solution(id_list, report, k):
-    answer = [0] * len(id_list)
-    declare = [0] * len(id_list)
+    answer = [0 for _ in range(len(id_list))]
     a = {}
-    for i in set(report):
-        x = i.split()
-        if x[0] in a.keys():
-            a[x[0]].add(x[1])
+    for i in report:
+        x, y = i.split()
+        if y in a.keys():
+            a[y].add(x)
         else:
-            a[x[0]] = {x[1]}
-        declare[id_list.index(x[1])] += 1
-    for i in range(len(declare)):
-        if declare[i] >= k:
-            for key, value in a.items():
-                if id_list[i] in value:
-                    answer[id_list.index(key)] += 1       
+            a[y] = {x}
+    
+    for y in a.values():
+        if len(y) >= k:
+            for i in y:
+                answer[id_list.index(i)] += 1
+    
     return answer
