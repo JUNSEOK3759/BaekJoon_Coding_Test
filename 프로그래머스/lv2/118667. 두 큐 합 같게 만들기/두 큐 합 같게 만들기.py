48a68e435babbc81794fb9a2f42a7d2925b92876
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-04-10 20:13:16 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 60.64 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -3,8 +3,11 @@ def solution(queue1, queue2):
     answer = 0
     deq1 = deque(queue1)
     deq2 = deque(queue2)
+    maxi = max(max(deq1), max(deq2))
     sum1 = sum(deq1)
     sum2 = sum(deq2)
+    if maxi > (sum1 + sum2) // 2:
+        return -1
     while sum1 != sum2:
         answer += 1
         if sum1 > sum2:
@@ -17,6 +20,6 @@ def solution(queue1, queue2):
             deq1.append(y)
             sum1 += y
             sum2 -= y
-        if answer == len(deq1) * 2:
+        if answer == len(queue1) * 3:
             return -1
     return answer
