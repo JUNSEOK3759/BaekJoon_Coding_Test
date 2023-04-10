c016e6521110c94dd1a3115f58ce4d1ea2d4979a
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-04-10 21:05:35 +0900

    [level 3] Title: 숫자 게임, Time: 48.36 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -4,12 +4,9 @@ def solution(A, B):
     A.sort()
     a = deque(A)
     B.sort()
-    while True:
-        for i in range(len(B)):
-            if B[i] > a[0]:
-                B[i] = 0
-                answer += 1
-                a.popleft()
-        else:
-            break    
+    for i in range(len(B)):
+        if B[i] > a[0]:
+            B[i] = 0
+            answer += 1
+            a.popleft() 
     return answer
