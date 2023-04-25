d6e86d7105f6912321c230ea427beec84b6be791
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-04-25 15:36:32 +0900

    [Gold IV] Title: 알파벳, Time: 1148 ms, Memory: 55048 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,40 +1,37 @@
 import sys
+import heapq
 from collections import deque
-import heapq as hq
+import itertools
 import copy
-
 # sys.stdin = open("input.txt", "rt")
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-# readline을 문자열에서 쓸 때는 rstrip()를 붙여줘라
-sys.setrecursionlimit(10**9)
 
-def bfs():
-    global cnt
+def bfs(x, y, d):
+    global res
     dq = set()
-    dq.add((0, 0, a[0][0]))
+    dq.add((x, y, d))
+    
     while dq:
         x, y, d = dq.pop()
-        
-        if len(d) > cnt:
-            cnt = len(d)
+        if len(d) > res:
+            res = len(d)
         
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            
             if nx > -1 and ny > -1 and nx < r and ny < c:
                 if a[nx][ny] not in d:
-                    dq.add((nx, ny, d+a[nx][ny]))
-
-
+                    dq.add((nx, ny, d + a[nx][ny]))
+                    
+        
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 r, c = map(int, input().split())
 
-a = [list(map(str, input())) for _ in range(r)]
+a = [list(map(str, input().rstrip())) for _ in range(r)]
 
-alp = [0 for _ in range(27)]
-alp[ord(a[0][0]) - 64] = 1
-cnt = 1
-bfs()
-
-print(cnt)
+res = -2147483647
+bfs(0, 0, a[0][0])
+print(res)
