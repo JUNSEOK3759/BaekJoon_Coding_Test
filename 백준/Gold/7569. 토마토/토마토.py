5f3d5034c744f372d1005e6cc1ad93f99fd4fe14
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-04-25 15:07:05 +0900

    [Gold V] Title: 토마토, Time: 2104 ms, Memory: 56848 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,43 +1,54 @@
 import sys
 from collections import deque
+import heapq as hq
+import copy
+import math
 # sys.stdin = open("input.txt", "rt")
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
 
-def bfs1():
+input = sys.stdin.readline
+# readline을 문자열에서 쓸 때는 rstrip()를 붙여줘라
+sys.setrecursionlimit(10**9)
+
+
+dx = [0, 0, -1, 1, 0, 0]
+dy = [0, 0, 0, 0, -1, 1]
+dz = [-1, 1, 0, 0, 0, 0]
+
+def bfs():
     while dq:
         x, y, z = dq.popleft()
         for i in range(6):
             nx = x + dx[i]
             ny = y + dy[i]
             nz = z + dz[i]
-
-            if nx > -1 and nx < len(a) and ny > -1 and nz > -1 and ny < len(a[0]) and nz < len(a[0][0]):
+            
+            if nx > -1 and ny > -1 and nz > -1 and nx < h and ny < n and nz < m:
                 if a[nx][ny][nz] == 0:
                     a[nx][ny][nz] = a[x][y][z] + 1
                     dq.append([nx, ny, nz])
-dx = [-1, 1, 0, 0, 0, 0]
-dy = [0, 0, -1, 1, 0, 0]
-dz = [0, 0, 0, 0, -1, 1]
+
 m, n, h = map(int, input().split())
-a = []
-for i in range(h):
-    a.append([list(map(int, input().split())) for _ in range(n)])
+
+a = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+
 dq = deque()
-for i in range(len(a)):
-    for j in range(len(a[0])):
-        for k in range(len(a[0][0])):
+
+for i in range(h):
+    for j in range(n):
+        for k in range(m):
             if a[i][j][k] == 1:
                 dq.append([i, j, k])
 
-bfs1()
+bfs()
+
 res = -2147483647
-for i in range(len(a)):
-    for j in range(len(a[0])):
-        for k in range(len(a[0][0])):
+for i in range(h):
+    for j in range(n):
+        for k in range(m):
             if a[i][j][k] == 0:
                 print(-1)
                 sys.exit(0)
             else:
                 res = max(res, a[i][j][k])
+
 print(res -1)
