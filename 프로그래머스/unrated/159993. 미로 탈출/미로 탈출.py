d3239abfa0c3870d485bd6b519c4dc0debcc0db0
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-04-28 22:26:51 +0900

    [unrated] Title: 미로 탈출, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -0,0 +1,64 @@
+from collections import deque
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1] 
+                
+                
+def solution(maps):
+    answer = 0
+    for i in range(len(maps)):
+        maps[i] = list(maps[i])
+    
+    len_x = len(maps)
+    len_y = len(maps[0])
+    cnt = 0
+    ch = [[0 for _ in range(len_y)] for _ in range(len_x)]
+    
+    def bfs_L(x, y):
+        dq = deque()
+        dq.append([x, y])
+        ch = [[0 for _ in range(len_y)] for _ in range(len_x)]
+        while dq:
+            x, y = dq.popleft()
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if nx > -1 and ny > -1 and nx < len_x and ny < len_y and ch[nx][ny] == 0:
+                    if maps[nx][ny] == 'O' or maps[nx][ny] == 'E':
+                        ch[nx][ny] = ch[x][y] + 1
+                        dq.append([nx, ny])
+                    elif maps[nx][ny] == 'L':
+                        return ch[x][y] + 1
+                    
+    def bfs_E(x, y):
+        dq = deque()
+        dq.append([x, y])
+        ch = [[0 for _ in range(len_y)] for _ in range(len_x)]
+        while dq:
+            x, y = dq.popleft()
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if nx > -1 and ny > -1 and nx < len_x and ny < len_y and ch[nx][ny] == 0:
+                    if maps[nx][ny] == 'O' or maps[nx][ny] == 'S':
+                        ch[nx][ny] = ch[x][y] + 1
+                        dq.append([nx, ny])
+                    elif maps[nx][ny] == 'E':
+                        return ch[x][y] + 1
+    L = 0    
+    for i in range(len_x):
+        for j in range(len_y):
+            if maps[i][j] == 'S':
+                L = bfs_L(i, j)
+                break
+    E = 0
+    for i in range(len_x):
+        for j in range(len_y):
+            if maps[i][j] == 'L':
+                E = bfs_E(i, j)
+                break
+                
+    
+    return L + E if L and E else -1
