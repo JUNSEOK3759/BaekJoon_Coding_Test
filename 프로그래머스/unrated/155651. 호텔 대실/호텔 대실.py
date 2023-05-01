b057f1b77f59bbc355023c2bb2e701038c3927e0
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-05-01 22:06:18 +0900

    [unrated] Title: 호텔 대실, Time: 65.35 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,21 @@
+def solution(book_time):
+    answer = 0
+    
+    book_time.sort(key = lambda x : (x[0], x[1]))
+    rent = []
+    for i in book_time:
+        q = list()
+        for j in i:
+            x, y = j.split(":")
+            q.append(int(x) * 60 + int(y))
+        rent.append(q)
+    x = []
+    x.append(rent[0])
+    for i in range(1, len(rent)):
+        for j in range(len(x)):
+            if rent[i][0] >= x[j][1] + 10:
+                x[j] = rent[i]
+                break
+        else:
+            x.append(rent[i])
+    return len(x)
