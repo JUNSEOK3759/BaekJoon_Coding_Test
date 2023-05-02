1f19c9b4210ce1aab16eaeb97f9b82db5cd4931f
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-05-02 22:11:48 +0900

    [Gold IV] Title: 빙산, Time: 3156 ms, Memory: 38208 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -8,17 +8,15 @@ import itertools
 input = sys.stdin.readline
 # readline을 문자열에서 쓸 때는 rstrip()를 붙여줘라
 sys.setrecursionlimit(10**9)
-# 가장 일찍 끝나는 회의를 먼저 하도록 정렬 해준다
 
 def bfs(x, y):
-    global n, m
-    cnt = 1
-    visit = [[0 for _ in range(m)] for _ in range(n)]
-    visit[x][y] = 1
-    
     dq = deque()
     dq.append([x, y])
+    cnt = 1
     
+    visit = [[0 for _ in range(m)] for _ in range(n)]
+    visit[x][y] = 1
+
     while dq:
         x, y = dq.popleft()
         
@@ -28,18 +26,16 @@ def bfs(x, y):
             
             if visit[nx][ny] == 0 and a[nx][ny] != 0:
                 dq.append([nx, ny])
-                visit[nx][ny] = 1
                 cnt += 1
+                visit[nx][ny] = 1        
     return cnt
-
 n, m = map(int, input().split())
+
 a = [list(map(int, input().split())) for _ in range(n)]
 ch = [[0 for _ in range(m)] for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
 ice = []
-
 for i in range(n):
     for j in range(m):
         if a[i][j] != 0:
@@ -52,9 +48,9 @@ while ice:
     if len(ice) != bfs(ice[0][0], ice[0][1]):
         answer = cnt
         break
-    
     cnt += 1
     melt = []
+    
     for i in range(len(ice)-1, -1, -1):
         x, y = ice[i]
         
@@ -64,15 +60,14 @@ while ice:
             
             if a[nx][ny] == 0:
                 ch[x][y] += 1
-                
+        
         if ch[x][y] > 0:
             melt.append([x, y, i])
-            
+    
     for x, y, i in melt:
         a[x][y] -= ch[x][y]
         if a[x][y] <= 0:
             a[x][y] = 0
             ice.pop(i)
         ch[x][y] = 0
-
 print(answer)
