ebb892e9cbc88eb1826a899126d1b7feb56ff72d
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-05-06 14:08:33 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 88 ms, Memory: 36488 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -25,27 +25,28 @@ def bfs():
         for i in range(1, 7):
             next = x + i
             if next <= 100:
-                if next in snake.keys():
-                    next = snake[next]
                 if next in ladder.keys():
                     next = ladder[next]
+                if next in snake.keys():
+                    next = snake[next]
                 if ch[next] == 0:
-                    dq.append(next)
                     ch[next] = 1
                     a[next] = a[x] + 1
-
+                    dq.append(next)
 n, m = map(int, input().split())
 
 ladder = {}
+snake = {}
+
 for _ in range(n):
     x, y = map(int, input().split())
     ladder[x] = y
-snake = {}
+
 for _ in range(m):
     x, y = map(int, input().split())
     snake[x] = y
 
-a = [0 for _ in range(100 + 10)]
-ch = [0 for _ in range(100 + 10)]
+a = [0 for _ in range(100 + 1)]
+ch = [0 for _ in range(100 + 1)]
 
 print(bfs())
