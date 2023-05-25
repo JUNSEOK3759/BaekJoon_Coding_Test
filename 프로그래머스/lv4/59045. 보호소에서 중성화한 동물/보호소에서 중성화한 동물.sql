2f8009c6d3612177c04b2c05fcd402e95f2e13ad
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-05-25 17:35:22 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,5 @@
 -- 코드를 입력하세요
-SELECT a.ANIMAL_ID, a.ANIMAL_TYPE, a.NAME
-from ANIMAL_INS a
-join ANIMAL_OUTS b
+select a.ANIMAL_ID, a.ANIMAL_TYPE, a.NAME
+from ANIMAL_INS a join ANIMAL_OUTS b
 on a.ANIMAL_ID = b.ANIMAL_ID
-where (a.SEX_UPON_INTAKE = 'Intact Male' or a.SEX_UPON_INTAKE = 'Intact Female') and (b.SEX_UPON_OUTCOME = 'Neutered Male' or b.SEX_UPON_OUTCOME = 'Spayed Female')
+where (a.SEX_UPON_INTAKE = 'Intact Male' and b.SEX_UPON_OUTCOME = 'Neutered Male') or (a.SEX_UPON_INTAKE = 'Intact Female' and b.SEX_UPON_OUTCOME = 'Spayed Female')
