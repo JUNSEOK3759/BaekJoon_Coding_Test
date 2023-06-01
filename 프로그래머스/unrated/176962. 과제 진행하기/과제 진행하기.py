a90fcaba9d99a85fd78e39d061c90ef6ed53c3c8
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-01 18:01:38 +0900

    [unrated] Title: 과제 진행하기, Time: 1.77 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
@@ -0,0 +1,27 @@
+from collections import deque
+def solution(plans):
+    answer = []
+    a = []
+    for i in plans:
+        x, y, z = i[0], i[1], i[2]
+        y1, y2 = y.split(':')
+        y1, y2,z = int(y1), int(y2), int(z)
+        summ = y1 * 60 + y2
+        a.append([x, summ, summ + z])
+    a.sort(key = lambda x : (x[1]))
+    a = deque(a)
+    stack = []
+    while a:
+        if len(a) >= 2 and a[0][2] > a[1][1]:
+            x, y, z = a.popleft()
+            stack.append([x, z- a[0][1]])
+
+        else:
+            x, y, z = a.popleft()
+            answer.append(x)
+            if stack:
+                i, j = stack.pop()
+                a.appendleft([i, z, z + j])
+            
+
+    return answer
