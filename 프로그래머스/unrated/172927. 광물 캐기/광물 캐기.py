d428aafb06d9dd8980419801f00b617d86b309d2
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-02 14:23:40 +0900

    [unrated] Title: 광물 캐기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.py
@@ -2,9 +2,6 @@ from collections import deque
 def solution(picks, minerals):
     answer = 0
     x = []
-    diamondScore = {'diamond' : 1, 'iron' : 1, 'stone' : 1}
-    ironScore = {'diamond' : 5, 'iron' : 1, 'stone' : 1}
-    stoneScore = {'diamond' : 25, 'iron' : 5, 'stone' : 1}
     cnt = 0 
     for i in range(0, len(minerals), 5):
         if cnt == sum(picks):
@@ -13,6 +10,9 @@ def solution(picks, minerals):
         x.append(a)
         cnt += 1
     x.sort(key = lambda x : (x.count('diamond'), x.count('iron'), x.count('stone')), reverse = True)
+    diamondScore = {'diamond' : 1, 'iron' : 1, 'stone' : 1}
+    ironScore = {'diamond' : 5, 'iron' : 1, 'stone' : 1}
+    stoneScore = {'diamond' : 25, 'iron' : 5, 'stone' : 1}
     for i in x:
         if picks[0] > 0:
             picks[0] -= 1
