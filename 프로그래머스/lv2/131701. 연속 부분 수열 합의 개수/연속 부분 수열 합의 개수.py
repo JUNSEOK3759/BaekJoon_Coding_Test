dafefa252f81606e94a850dd1acac10ad4230aea
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-11 18:54:47 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 7038.44 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,37 +1,11 @@
 def solution(elements):
-    # answer = set(elements)
-    # a = 2
-    # while a <= 5:
-    #     for i in range(len(elements)):
-    #         elements2 = elements[i : i+a]
-    #         ele2_len = len(elements2)
-    #         if ele2_len < a:
-    #             elements2 = elements[i : i+a] + elements[0 :a - ele2_len]
-    #         answer.add(sum(elements2))
-    #     a+= 1
-    # return len(answer)
-    
-    
-    
-    elements *= 2
+    answer = 0
+    q = elements * 2
     a = 1
     x = set()
-    while a <= len(elements) // 2:
-        for i in range(len(elements) // 2 + 1):
-            x.add(sum(elements[i : i+a]))
-        a+=1
+    while a <= len(elements):
+        for i in range(len(q)):
+            x.add(sum(q[i : i+a]))
+        a += 1
     return len(x)
-
-
-
-   
     
-    
-#     answer = 0
-#     x = []
-#     for i in range(2, len(elements) + 1):
-#         for j in range(0, len(elements) -1, i):
-#             print(elements[j : j+i])
-#             x.append(sum(elements[j : j+i]))
-    
-#     return answer
