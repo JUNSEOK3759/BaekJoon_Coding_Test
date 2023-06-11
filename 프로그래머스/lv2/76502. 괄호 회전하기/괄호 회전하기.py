3261f8328665e256aad6aa2ed552af6325102695
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-11 17:59:46 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,25 +1,22 @@
 from collections import deque
 def solution(s):
     answer = 0
-    a = 0
-    x = deque()
-    while a < len(s):
-        s = s[1 : ] + s[0]
+    s = deque(s)
+    def asd(s):
+        stack = []
         for i in s:
-            x.append(i)
-            if len(x) >= 2:
-                if x[-1] == '}' and x[-2] == '{':
-                    x.pop()
-                    x.pop()
-                elif x[-1] == ')' and x[-2] == '(':
-                    x.pop()
-                    x.pop()
-                elif x[-1] == ']' and x[-2] == '[':
-                    x.pop()
-                    x.pop()
-        if len(x) == 0:
+            stack.append(i)
+            while len(stack) >= 2 and (ord(stack[-1]) == ord(stack[-2])+1 or ord(stack[-1]) == ord(stack[-2])+2):
+                stack.pop()
+                stack.pop()
+        return stack
+    cnt = 0
+    while cnt < len(s):
+        s.append(s.popleft())
+        res = asd(s)
+        if not res:
+
             answer += 1
-        else:
-            x = deque()
-        a+=1
+        cnt += 1
+        
     return answer
