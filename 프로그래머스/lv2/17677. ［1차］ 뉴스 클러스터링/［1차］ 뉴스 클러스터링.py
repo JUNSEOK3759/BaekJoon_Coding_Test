9333a846d825826223dc94eb64941c1203a6935b
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-11 22:21:51 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,15 +1,17 @@
 from collections import Counter
+import re
+import math
 def solution(str1, str2):
-    
-    s1 = str1.lower()
-    s2 = str2.lower()
+    str1, str2 = str1.lower(), str2.lower()
     a, b = [], []
-    for i in range(len(s1)-1):
-        if s1[i].isalpha() and s1[i+1].isalpha():
-            a.append(s1[i]+s1[i+1])
-    for i in range(len(s2)-1):
-        if s2[i].isalpha() and s2[i+1].isalpha():
-            b.append(s2[i]+s2[i+1])
+    
+    for i in range(len(str1)-1):
+        if str1[i].isalpha() and str1[i+1].isalpha():
+            a.append(str1[i] + str1[i+1])
+    for i in range(len(str2)-1):
+        if str2[i].isalpha() and str2[i+1].isalpha():
+            b.append(str2[i] + str2[i+1])
+            
     x = Counter(a)
     y = Counter(b)
     
