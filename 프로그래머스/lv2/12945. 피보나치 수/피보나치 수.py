e5da6e958bf60e70c3c5c83ca1b723901548e2c9
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-11 16:19:08 +0900

    [level 2] Title: 피보나치 수, Time: 88.80 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,5 @@
 def solution(n):
-    a = [0 for _ in range(n+1)]
-    a[1] = 1
-    
-    for i in range(2, n+1):
-        a[i] = a[i-1] + a[i-2]
-    return a[-1] % 1234567
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a+b
+    return a % 1234567
