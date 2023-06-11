585852e403f4f806b98fb6e9482dd7397a569693
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-11 15:37:50 +0900

    [level 2] Title: 올바른 괄호, Time: 22.81 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,6 @@
 def solution(s):
-    stack = []
+    answer = True
     
-    for i in s:
-        stack.append(i)
-        while len(stack) >= 2 and stack[-1] == ')' and stack[-2] == '(':
-            stack.pop()
-            stack.pop()
-        
-    if stack:
-        return False
-    else:
-        return True
+    print(divmod(8, 4))
+
+    return True
