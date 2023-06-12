bec448a6b7a7adb2fe0aa8faf1598d45e4ec01b0
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-12 23:55:37 +0900

    [unrated] Title: 귤 고르기, Time: 34.46 ms, Memory: 22.3 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,14 +1,15 @@
 from collections import Counter
 def solution(k, tangerine):
     answer = 0
-    x = Counter(tangerine)
+    s = Counter(tangerine)
     a = []
-    for x, y in x.items():
+    for x, y in s.items():
         a.append([x, y])
-    a.sort(key = lambda x : (x[1]), reverse = True)
     cnt = 0
+    a.sort(key = lambda x : x[1], reverse = True)
     while k > 0:
         k -= a[cnt][1]
-        answer += 1
         cnt += 1
+        answer += 1
+    
     return answer
