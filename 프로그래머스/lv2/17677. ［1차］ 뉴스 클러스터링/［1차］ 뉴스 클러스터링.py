7e3d17b9b63b7da4f8d8011e2c1fa39c0e7681a3
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-13 00:10:26 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.17 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,21 +1,20 @@
 from collections import Counter
-import re
-import math
 def solution(str1, str2):
-    str1, str2 = str1.lower(), str2.lower()
+    
+    str1 , str2 = str1.lower(), str2.lower()
     a, b = [], []
     
     for i in range(len(str1)-1):
         if str1[i].isalpha() and str1[i+1].isalpha():
-            a.append(str1[i] + str1[i+1])
+            a.append(str1[i]+str1[i+1])
     for i in range(len(str2)-1):
         if str2[i].isalpha() and str2[i+1].isalpha():
-            b.append(str2[i] + str2[i+1])
-            
-    x = Counter(a)
-    y = Counter(b)
-    
-    inner = list((x & y).elements())
-    union = list((x | y).elements())
-    
-    return 65536 if not inner and not union else int(len(inner) / len(union) * 65536)
+            b.append(str2[i]+str2[i+1])
+    a, b = Counter(a), Counter(b)
+    print('a :', a)
+    print('b :', b)
+    x = list((a&b).elements())
+    y = list((a|b).elements())
+    print(f'x : {x}')
+    print(f'y : {y}')
+    return int(len(x) / len(y) * 65536) if x or y else 65536
