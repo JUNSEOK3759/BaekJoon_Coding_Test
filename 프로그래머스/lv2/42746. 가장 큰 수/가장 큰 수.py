54ac94b4a3216d711364db270e56953fa85beee4
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-14 01:58:52 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     answer = ''
-    numbers.sort(key = lambda x : ((str(x)*3)[:4]), reverse = True)
+    numbers.sort(key = lambda x : ((str(x)*3)), reverse = True)
     for i in numbers:
         answer += str(i)
     return str(int(answer))
