a15aa4c8c6268f43ed4877ecfc23621e7b5ed88f
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-13 12:34:54 +0900

    [level 2] Title: 할인 행사, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -4,13 +4,11 @@ def solution(want, number, discount):
     a = Counter()
     for i in range(len(want)):
         a[want[i]] = number[i]
-    a = a.most_common()
-    a.sort(key = lambda x : x[0])
+
 
     for i in range(len(discount)):
         x = Counter(discount[i:i+sum(number)])
-        x = x.most_common()
-        x.sort(key = lambda x : x[0])
+
         if a == x:
             answer += 1
     
