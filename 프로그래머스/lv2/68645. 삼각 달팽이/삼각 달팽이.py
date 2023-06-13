6ec04369f441032c9cfc0bfb2e79c4f370f092c4
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-13 17:04:20 +0900

    [level 2] Title: 삼각 달팽이, Time: 250.13 ms, Memory: 43 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,9 +1,10 @@
 def solution(n):
-    res = [[0 for _ in range(n)] for _ in range(n)]    
+    answer = []
+    res = [[0 for _ in range(n)] for _ in range(n)]
     x, y = -1, 0
     num = 1
     for i in range(n):
-        for j in range(i, n):
+        for _ in range(i, n):
             if i % 3 == 0:
                 x += 1
             elif i % 3 == 1:
@@ -13,9 +14,5 @@ def solution(n):
                 y -= 1
             res[x][y] = num
             num += 1
-    tot = []
-    for i in res:
-        for j in i:
-            if j != 0:
-                tot.append(j)
-    return tot
+
+    return [j for i in res for j in i if j != 0]
