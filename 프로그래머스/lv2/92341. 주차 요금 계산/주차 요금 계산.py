f072de05e3da7c5f20aee9050a4981d3e3af87f5
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-13 15:32:53 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,31 +1,38 @@
-def dictionary(records):
-    dic = {}
-    for i in records:
-        x, y, z = i.split()
-        if y in dic:
-            dic[y].append(x)
-        else:
-            dic[y] = [x]
-    dic = sorted(dic.items())
-    return dict(dic)
-
 import math
 def solution(fees, records):
+    
+    def timer(x):
+        a, b = x.split(':')
+        y = int(a) * 60 + int(b)
+        return y
+    
     answer = []
-    dic = dictionary(records)
-    print(dic)
-    for key, value in dic.items():
-        for i in range(len(value)):
-            t, m = value[i].split(':')
-            value[i] = (int(t)*60) + int(m)
-        tot = 0
-        for i in range(0, len(value), 2):
-            try:
-                tot += value[i+1] -value[i]
-            except IndexError:
-                tot += (60*24) - 1 - value[i]
-        if tot > fees[0]:       
-            answer.append(fees[1] + math.ceil((tot - fees[0]) / fees[2]) *fees[3])
+    a = {}
+    for i in records:
+        x, y, z = i.split()
+        x = timer(x)
+        if y not in a:
+            a[y] = [x]
         else:
-            answer.append(fees[1])
+            a[y].append(x)
+    res = []
+    
+    for x, y in a.items():
+        q = [x]
+        w = 0
+        if len(y) % 2 != 0:
+            a[x].append(60*24 - 1)
+        for i in range(0, len(y), 2):
+            w += abs(y[i+1] - y[i])
+        if w > fees[0]:
+            w = math.ceil((w-fees[0]) / fees[2]) * fees[3]
+        else:
+            w = 0
+        q.append(fees[1] + w)
+        res.append(q)
+    res.sort()
+    
+    for i in res:
+        answer.append(i[1])
+    
     return answer
