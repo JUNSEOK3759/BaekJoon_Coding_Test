89c48c784edcff12ce4cc7cff91fe348e73d5894
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-14 19:21:05 +0900

    [Silver IV] Title: 문자열, Time: 88 ms, Memory: 36856 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.py
@@ -0,0 +1,25 @@
+from collections import deque, Counter
+import heapq as hq
+import copy
+import math
+import itertools
+
+# import re
+import sys
+# sys.stdin = open("input.txt", "rt")
+
+input = sys.stdin.readline
+# readline을 문자열에서 쓸 때는 rstrip()를 붙여줘라
+sys.setrecursionlimit(10**9)
+
+a, b = map(str, input().split())
+
+tot = 2147483647
+
+for i in range(len(b) - len(a) + 1):
+    count = 0
+    for j in range(len(a)):
+        if a[j] != b[i+j]:
+            count += 1
+    tot = min(tot, count)
+print(tot)
