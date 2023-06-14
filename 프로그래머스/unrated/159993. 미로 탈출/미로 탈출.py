e796a44f89fd71776a8d005e16903968f9ffb7f9
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-14 17:35:51 +0900

    [unrated] Title: 미로 탈출, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -1,20 +1,12 @@
 from collections import deque
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1] 
-                
-                
 def solution(maps):
     answer = 0
-    for i in range(len(maps)):
-        maps[i] = list(maps[i])
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    len_x, len_y = len(maps), len(maps[0])
     
-    len_x = len(maps)
-    len_y = len(maps[0])
-    cnt = 0
-    ch = [[0 for _ in range(len_y)] for _ in range(len_x)]
     
-    def bfs_L(x, y):
+    def lbfs(x, y):
         dq = deque()
         dq.append([x, y])
         ch = [[0 for _ in range(len_y)] for _ in range(len_x)]
@@ -24,43 +16,34 @@ def solution(maps):
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                if nx > -1 and ny > -1 and nx < len_x and ny < len_y and ch[nx][ny] == 0:
+                
+                if -1 < nx < len_x and -1 < ny < len_y and ch[nx][ny] == 0:
                     if maps[nx][ny] == 'O' or maps[nx][ny] == 'E':
                         ch[nx][ny] = ch[x][y] + 1
                         dq.append([nx, ny])
                     elif maps[nx][ny] == 'L':
                         return ch[x][y] + 1
-                    
-    def bfs_E(x, y):
+                
+    def ebfs(x, y):
         dq = deque()
         dq.append([x, y])
-        ch = [[0 for _ in range(len_y)] for _ in range(len_x)]
+        ch2 = [[0 for _ in range(len_y)] for _ in range(len_x)]
         while dq:
             x, y = dq.popleft()
             
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                if nx > -1 and ny > -1 and nx < len_x and ny < len_y and ch[nx][ny] == 0:
+                if -1 < nx < len_x and -1 < ny < len_y and ch2[nx][ny] == 0:
                     if maps[nx][ny] == 'O' or maps[nx][ny] == 'S':
-                        ch[nx][ny] = ch[x][y] + 1
+                        ch2[nx][ny] = ch2[x][y] + 1
                         dq.append([nx, ny])
                     elif maps[nx][ny] == 'E':
-                        return ch[x][y] + 1
-    L = 0
-    E = 0
+                        return ch2[x][y] + 1
     for i in range(len_x):
         for j in range(len_y):
             if maps[i][j] == 'S':
-                L = bfs_L(i, j)
-            elif maps[i][j] == 'L':
-                E = bfs_E(i, j)
-    
-    # for i in range(len_x):
-    #     for j in range(len_y):
-    #         if maps[i][j] == 'L':
-    #             E = bfs_E(i, j)
-    #             break
-                
-    
+                L = lbfs(i,j)
+            if maps[i][j] == 'L':
+                E = ebfs(i, j)
     return L + E if L and E else -1
