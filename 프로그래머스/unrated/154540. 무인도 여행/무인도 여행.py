4af9373064ec400e616bc8cd19b5505bf03cbd78
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-14 16:54:44 +0900

    [unrated] Title: 무인도 여행, Time: 0.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -1,37 +1,32 @@
 from collections import deque
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
 def solution(maps):
     answer = []
-    for i in range(len(maps)):
-        maps[i] = list(maps[i])
-    cnt = 0
-    n = len(maps)
-    m = len(maps[0])
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    len_x = len(maps)
+    len_y = len(maps[0])
+    ch = [[0 for _ in range(len_y)] for _ in range(len_x)]
     def bfs(x, y):
         dq = deque()
         dq.append([x, y])
         cnt = int(maps[x][y])
         while dq:
             x, y = dq.popleft()
+            
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
-                
-                if -1 < nx < n and -1 < ny < m and ch[nx][ny] == 0 and maps[nx][ny].isdigit():
-                    ch[nx][ny] = 1
-                    dq.append([nx, ny])
-                    cnt += int(maps[nx][ny])
-                    
+                if -1 < nx < len_x and -1 < ny < len_y and maps[nx][ny] != 'X':
+                    if ch[nx][ny] == 0:
+                        ch[nx][ny] = 1
+                        cnt += int(maps[nx][ny])
+                        dq.append([nx,ny])
         return cnt
-    res = []
-    ch = [[0 for _ in range(m)] for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            if maps[i][j].isdigit() and ch[i][j] == 0:
-                ch[i][j] = 1
-                res.append(bfs(i, j))
-    res.sort()
     
-    return res if res else [-1]
+    for i in range(len_x):
+        for j in range(len_y):
+            if maps[i][j] != 'X' and ch[i][j] == 0:
+                ch[i][j] = 1
+                answer.append(bfs(i, j))
+            
+    return sorted(answer) if answer else [-1]
