d8227eaff4632ba8f0fe506f7f64c62e9a0b65cb
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-15 16:02:36 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,15 +1,13 @@
 def solution(id_list, report, k):
     answer = [0 for _ in range(len(id_list))]
     a = {}
-    
     for i in report:
-        x, y = i.split()
-        if y in a.keys():
+        x, y = i.split(' ')
+        if y in a:
             a[y].add(x)
         else:
             a[y] = {x}
-
-    for y in a.values():
+    for x, y in a.items():
         if len(y) >= k:
             for i in y:
                 answer[id_list.index(i)] += 1
