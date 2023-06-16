093954a6519e472961271b697edb437ca778d3ec
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-16 14:44:01 +0900

    [level 2] Title: [3차] 방금그곡, Time: 3.39 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -0,0 +1,22 @@
+import re
+def solution(m, musicinfos):
+    answer = [0, 0]
+    for i in musicinfos:
+        x, y, z, w = i.split(',')
+        for i in range(65, 73):
+            w = re.sub(f'{chr(i)}#', chr(i+32), w)
+            m = re.sub(f'{chr(i)}#', chr(i+32), m)
+        x1, x2 = x.split(':')
+        y1, y2 = y.split(':')
+        x = int(x1) * 60 + int(x2)
+        y = int(y1) * 60 + int(y2) 
+        time = int(y-x)
+        len_w = len(w)
+        div = time // len_w
+        mod = time % len_w
+        res = w * div + w[:mod]
+        if m in res:
+            if len(res) > answer[1]:
+                answer = [z,len(res)]
+    
+    return answer[0] if answer != [0, 0] else '(None)'
