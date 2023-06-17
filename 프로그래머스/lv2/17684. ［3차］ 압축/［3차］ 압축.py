9d6fd8e13606d76ca0064b2e10f013a94922759f
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-17 21:22:13 +0900

    [level 2] Title: [3차] 압축, Time: 0.28 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,15 +1,19 @@
 def solution(msg):
     answer = []
-    a = {chr(i) : i-64 for i in range(65, 65+26)}
+    alpha = {chr(i): i-64 for i in range(65, 65 + 26)}
+    
     lt = 0
     rt = 1
+    
     while True:
         if rt == len(msg):
-            answer.append(a[msg[lt:rt]])
+            answer.append(alpha[msg[lt:rt]])
             break
-        if msg[lt:rt+1] not in a:
-            a[msg[lt:rt+1]] = len(a) + 1
-            answer.append(a[msg[lt:rt]])
+        if msg[lt : rt+1] not in alpha:
+            answer.append(alpha[msg[lt:rt]])
+            alpha[msg[lt:rt+1]] = len(alpha) + 1
             lt = rt
         rt += 1
+    
     return answer
+
