526428b4cc9fca6b05bad757500f99f5bd48fffd
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-17 22:24:24 +0900

    [unrated] Title: 롤케이크 자르기, Time: 552.49 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -3,9 +3,8 @@ def solution(topping):
     answer = 0
     a = Counter(topping)
     x = set()
-    
     for i in topping:
-        a[i] -=1
+        a[i] -= 1
         x.add(i)
         if a[i] == 0:
             a.pop(i)
