db6265bb71e2662b91cb108c55192c17aed99d24
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-20 18:06:08 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.48 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,28 @@
+from collections import deque
+import re
+def firstLastDot(x):
+    if x[0] == '.':
+        x.popleft()
+    elif x[-1] == '.':
+        x.pop()
+    if x:
+        return ''.join(x)
+    else:
+        return 'a'
+def solution(new_id):
+    n = new_id.lower()
+    x = ''
+    for i in n:
+        if i.isdigit() or i.isalpha() or i in ['-','_','.']:
+            x+= i
+    x = deque(x)
+    for i in range(len(x)-1):
+        if x[i] == '.' and x[i+1] == '.':
+            x[i] = ' '
+    x = re.sub(r"\s", '', ''.join(x))
+    x = firstLastDot(deque(x))
+    x = x[:15]
+    x = firstLastDot(deque(x))
+    while len(x) < 3:
+        x += x[-1]
+    return x
