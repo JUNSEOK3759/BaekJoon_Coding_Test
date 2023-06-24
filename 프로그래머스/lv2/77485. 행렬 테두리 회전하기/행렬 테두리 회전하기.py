1d98a7e9188e2a6d05bef2f593a49dabddb7c12c
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-24 17:57:52 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 263.83 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -0,0 +1,31 @@
+def solution(rows, columns, queries):
+    answer = []
+    a = [[(columns*j)+i+1 for i in range(columns)] for j in range(rows)]
+    for i in queries:
+        x1, y1, x2, y2 = i
+        x1, y1, x2, y2 = x1-1 , y1-1 , x2-1 , y2-1 
+        y21 = a[x1][y2]
+        y11 = a[x2][y1]
+        mini = 2147483647
+        for j in range(y2, y1, -1):
+            a[x1][j] = a[x1][j-1]
+            mini = min(a[x1][j], mini)
+        for j in range(y1, y2):
+            a[x2][j] = a[x2][j+1]
+            mini = min(a[x2][j], mini)
+        for j in range(x1, x2):
+            a[j][y1] = a[j+1][y1]
+            mini = min(a[j][y1], mini)
+        for j in range(x2, x1, -1):
+            a[j][y2] = a[j-1][y2]
+            mini = min(a[j][y2], mini)
+        a[x1+1][y2] = y21
+        a[x2-1][y1] = y11
+        
+        mini = min(mini, y21, y11)
+        
+        answer.append(mini)
+        
+        
+        
+    return answer
