4340d094f1bb663352426e110b40247d6bcd2b07
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-24 18:18:02 +0900

    [unrated] Title: 대충 만든 자판, Time: 400.93 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,17 +1,16 @@
 def solution(keymap, targets):
     answer = []
+    
     for i in targets:
         res = 0
         for j in i:
-            cnt = 2147483647
-            for k in range(len(keymap)):
-                for l in range(len(keymap[k])):
-                    if j == keymap[k][l]:
-                        if l+1 < cnt:
-                            cnt = l+1
-                        break
-    
-            res += cnt 
-        answer.append(res if res < 2147483647 else -1)
-                        
-    return answer 
+            mini = 2147000000
+            for k in keymap:
+                k = list(k)
+                if j in k:
+                    mini = min(k.index(j)+1, mini)
+            res += mini
+        if res >= 2147000000:
+            res = -1
+        answer.append(res)
+    return answer
