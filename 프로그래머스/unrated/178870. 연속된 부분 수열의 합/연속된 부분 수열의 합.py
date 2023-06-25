ff6b9398eb315f1370c7b929c6197a68abf1d0bb
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-25 09:00:40 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,30 +1,29 @@
-from collections import deque
 def solution(a, k):
     answer = []
     
     if k in a:
         x = a.index(k)
         return [x, x]
-    sum = a[0]
+    tot = a[0]
     lt = 0
     rt = 0
+    
     while True:
         if lt == len(a)-1:
             break
-        if sum < k:
-            if rt < len(a)-1:
-                rt += 1
-                sum += a[rt]
-            else:
-                break
-        elif sum > k:
-            sum -= a[lt]
+        if tot == k:
+            answer.append([lt, rt])
+            tot -= a[lt]
             lt += 1
             
+        elif tot < k:
+            if rt < len(a)-1:
+                rt += 1
+                tot += a[rt]
+            else:
+                break
         else:
-            if not answer or rt - lt < answer[1] - answer[0]:
-                answer = [lt, rt]
-            sum -= a[lt]
+            tot -= a[lt]
             lt += 1
-                
-    return answer
+    answer.sort(key = lambda x : ((x[1]-x[0], x[0])))
+    return answer[0]
