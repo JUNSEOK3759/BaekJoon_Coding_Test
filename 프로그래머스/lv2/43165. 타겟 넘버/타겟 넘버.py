c8a9b59109b971e892404248558bbad3af81e382
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-29 11:58:57 +0900

    [level 2] Title: 타겟 넘버, Time: 2.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,9 +1,14 @@
+answer = 0
 def solution(numbers, target):
-    head = [0]
-    for i in numbers:
-        heap = []
-        for j in head:
-            heap.append(j+i)
-            heap.append(j-i)
-        head = heap
-    return head.count(target)
+    n = len(numbers)
+    def dfs(l, summ):
+        global answer
+        if l == n:
+            if summ == target:
+                answer += 1
+            return 
+        else:
+            dfs(l+1, summ + numbers[l])
+            dfs(l+1, summ - numbers[l])
+    dfs(0, 0)
+    return answer
