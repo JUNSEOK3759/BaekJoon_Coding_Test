3e07a77a27b921456fb55a29c272cd773b99d29d
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-06-29 21:14:40 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,14 +1,12 @@
 def solution(wallpaper):
-    answer = [2147483647, 2147483647, -2147483647, -2147483647]
+    answer = [2147483647, 2147483647,-2147483647, -2147483647]
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[0])):
             if wallpaper[i][j] == '#':
-                if i < answer[0]:  
-                    answer[0] = i
-                if j < answer[1]:
-                    answer[1] = j
-                if i+1 > answer[2]:
-                    answer[2] = i+1
-                if j+1 > answer[3]:
-                    answer[3] = j+1
+                answer[0] = min(answer[0], i)
+                answer[1] = min(answer[1], j)
+                answer[2] = max(answer[2], i)
+                answer[3] = max(answer[3], j)
+    answer[2] += 1
+    answer[3] += 1
     return answer
