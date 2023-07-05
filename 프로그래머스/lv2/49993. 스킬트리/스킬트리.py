22e7c8ef48cd3f826de33ff07a2b4384700d549a
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-07-05 22:07:38 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -3,10 +3,10 @@ def solution(skill, skill_trees):
     answer = 0
     
     skill_list = list(skill)
-    for i in range(len(skill_trees)):
+    for i in skill_trees:
         a = {}
-        for j in range(len(skill_trees[i])):
-            x = skill_trees[i][j]
+        for j in range(len(i)):
+            x = i[j]
             if x in skill_list:
                 a[j] = x
         x = ''.join([i for i in a.values()])
