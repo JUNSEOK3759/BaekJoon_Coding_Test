de6ef9e6f1a4897334064ff643be69910f44ab06
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-07-06 18:41:38 +0900

    [unrated] Title: 옹알이 (2), Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,21 +1,11 @@
-import re
 def solution(babbling):
     answer = 0
     a = ["aya", "ye", "woo", "ma"]
-    b = [0, 0, 0, 0]
-    for i in a:
-        x = []
-        for j in babbling:
-            if i*2 in j:
-                x.append(j)
-        for j in x:
-            babbling.remove(j)
-    for i in range(len(babbling)):
-        cnt = 0
-        while cnt < len(a):
-            if a[cnt] in babbling[i]:
-                babbling[i] = re.sub(a[cnt], 'X', babbling[i])
-            else:
-                cnt += 1
-        babbling[i] = re.sub('X', '', babbling[i])
-    return babbling.count('')
+    
+    for i in babbling:
+        for j in a:
+            if j*2 not in i:
+                i = i.replace(j, ' ')
+        if len(i.strip()) == 0:
+            answer += 1
+    return answer
