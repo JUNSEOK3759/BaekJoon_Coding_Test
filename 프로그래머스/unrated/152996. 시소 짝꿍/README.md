f2befa69ea07cb8795568177f4d5fb0a2d7d6c54
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-07-06 13:52:56 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/README.md
+++ 프로그래머스/unrated/152996. 시소 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [unrated] 시소 짝꿍 - 152996 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152996#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,17 +1,26 @@
-from collections import Counter
-def solution(weights):
+import itertools
+def solution(a):
     answer = 0
-    a = Counter(weights)
-    
-    for i in a:
-        x = a[i]
+    a.sort()
+    cnt = 0
+    diction = {}
+    while cnt < len(a):
+        x = a.count(a[cnt])
+        diction[a[cnt]] = x
         if x > 1:
-            answer += (x *(x-1)) // 2
-        if i*2 in a:
-            answer += x * a[i*2]
-        if i * 2/3 in a:
-            answer += x * a[i*(2/3)]
-        if i * 3/4 in a:
-            answer += x * a[i*(3/4)]
-    
+            if x == 2:
+                answer += 1
+            else:
+                answer += x * (x-1) // 2
+            cnt += x
+        else:
+            cnt += 1
+    l, m, n = 4, 3, 2 
+    a = sorted(list(set(a)))
+    for com in itertools.combinations(a, 2):
+        q, w = com
+        if q == w:
+            continue
+        elif q * l == w * m or q * l == w * n or q * m == w * n:
+            answer += diction[q] * diction[w]
     return answer
