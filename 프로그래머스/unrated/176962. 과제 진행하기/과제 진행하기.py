41c747ce1e5a6acd4b11aa74e9eba086f38c1360
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-07-11 19:03:36 +0900

    [unrated] Title: 과제 진행하기, Time: 2.90 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
@@ -3,31 +3,28 @@ def solution(plans):
     answer = []
     
     def makeNewList(plans):
-        a = []
-        for i in plans:
-            x, y, z = i[0], i[1], i[2]
-            y1, y2 = y.split(':')
-            y1, y2,z = int(y1), int(y2), int(z)
-            summ = y1 * 60 + y2
-            a.append([x, summ, summ + z])
-        return a
+        for i in range(len(plans)):
+            x, y, z = plans[i]
+            a, b = y.split(':')
+            y = int(a) * 60 + int(b)
+            z = int(z)
+            plans[i] = [x, y, y+z]
+        
     
-    a = makeNewList(plans)
-    a.sort(key = lambda x : (x[1]))
-    a = deque(a)
+    makeNewList(plans)
+    plans.sort(key = lambda x : x[1])
+    a = deque(plans)
     stack = []
-
+    
     while a:
         if len(a) >= 2 and a[0][2] > a[1][1]:
             x, y, z = a.popleft()
             stack.append([x, z - a[0][1]])
-
         else:
             x, y, z = a.popleft()
             answer.append(x)
             
             if stack:
                 i, j = stack.pop()
-                a.appendleft([i, z, z + j])
-                
+                a.appendleft([i, z, z+j])
     return answer
