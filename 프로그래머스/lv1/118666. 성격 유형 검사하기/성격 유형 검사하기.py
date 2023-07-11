aa4395cec541e5a536af3247d24fda7d6e378e32
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-07-11 17:52:51 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.45 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,18 +1,27 @@
+'''
+1 : r, t
+2 : c, f
+3 : j, m
+4 : a, n
+'''
 def solution(survey, choices):
     answer = ''
-    x = ['R','T','C','F','J','M','A','N']
-    a = {'R' : 0, 'T' : 0,
-         'C' : 0, 'F' : 0,
-         'J' : 0, 'M' : 0,
-         'A' : 0, 'N' : 0,}
+    a = {'R' : 0, 'T' : 0, 'C' : 0, 'F' : 0, 'J' : 0, 'M' : 0, 'A' : 0, 'N' : 0}
+    
     for i in range(len(survey)):
-        if choices[i] > 4:
-            a[survey[i][1]] += choices[i] - 4
+        x, y = survey[i][0], survey[i][1]
+        z = choices[i]
+        if z>= 5:
+            a[y] += z-4
+        elif z < 4:
+            a[x] += 4 - z
+    
+    res = [i for i in a.values()]
+    alp = [i for i in a.keys()]
+    
+    for i in range(0, len(res), 2):
+        if res[i] >= res[i+1]:
+            answer += alp[i]
         else:
-            a[survey[i][0]] += 4 - choices[i]
-    for i in range(0, len(x), 2):
-        if a[x[i]] >= a[x[i+1]]:
-            answer += x[i]
-        else:
-            answer += x[i+1]
+            answer += alp[i+1]
     return answer
