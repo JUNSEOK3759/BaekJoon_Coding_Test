1cd19ef8c2e643df55778924ec976fef569be517
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-07-29 22:35:14 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.12 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,14 +1,16 @@
+import math
 def solution(number, limit, power):
-    x = 0
+    answer = 0
+    
     for i in range(1, number+1):
         result = 0
-        for j in range(1, int(i**0.5)+1):
+        for j in range(1, int(math.sqrt(i))+1):
             if i % j == 0:
                 result += 1
-                if j ** 2 != i:
+                if j**2 != i:
                     result += 1
             if result > limit:
                 result = power
                 break
-        x += result
-    return x
+        answer += result
+    return answer
