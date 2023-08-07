917c1e06e9647401487ec826d55c4330774a6cf2
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-08-07 15:20:21 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,37 @@
+answer = 0
+
+def dfs(l, s, target, words, n, ch):
+    global answer
+    if l == n:
+        return 0
+    if s == target:
+        return
+    else:
+        for i in range(len(words)):
+            if difference(words[i], s) == 1 and ch[i] == 0:
+                ch[i] = 1
+                answer += 1
+                dfs(l+1, words[i], target, words, n, ch)
+                break
+        else:
+            return 0
+            
+def difference(x, y):
+        dif = 0
+        for i in range(len(x)):
+            if x[i] != y[i]:
+                dif += 1
+        return dif
+            
+def solution(begin, target, words):
+    
+    if target not in words:
+        return 0
+    else:
+        words.remove(target)
+        words.insert(0, target)
+    
+    n = len(words)
+    ch = [0 for _ in range(len(words))]
+    dfs(0, begin, target, words, n, ch)
+    return answer
