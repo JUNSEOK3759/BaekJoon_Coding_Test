b89b8f2efbe7e77bea5fcfb1cfae49475060c2f8
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-08-15 22:45:21 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -3,14 +3,12 @@ def solution(storey):
     
     while storey:
         x = storey % 10
-        
         if x > 5:
             answer += (10 - x)
             storey += 10
         elif x < 5:
             answer += x
-        
-        else:       
+        else:
             if (storey // 10) % 10 > 4:
                 storey += 10
             answer += x
