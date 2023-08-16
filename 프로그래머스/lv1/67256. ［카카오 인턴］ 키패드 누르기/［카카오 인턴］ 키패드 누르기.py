0b1169fdfea5a44a56c9470540b5d091e82b05a7
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-08-16 15:04:26 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.38 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,33 +1,32 @@
 def solution(numbers, hand):
     answer = ''
-    pad = {'1' : [0, 0], '2' : [0, 1], '3' : [0, 2],
-           '4' : [1, 0], '5' : [1, 1], '6' : [1, 2],
-           '7' : [2, 0], '8' : [2, 1], '9' : [2, 2],
-           '*' : [3, 0], '0' : [3, 1], '#' : [3, 2]
-          }
-    left = pad['*']
-    right = pad['#']
-    for i in numbers:
-        if i in [1, 4, 7]:
+    pad = {1 : [0, 0], 2 : [0, 1], 3 : [0, 2],
+           4 : [1, 0], 5 : [1, 1], 6 : [1, 2],
+           7 : [2, 0], 8 : [2, 1], 9 : [2, 2],
+           '*' : [3, 0], 0 : [3, 1], '#' : [3, 2]}
+    left, right = '*', '#'
+    for num in numbers:
+        if num in [1, 4, 7]:
             answer += 'L'
-            left = pad[str(i)]
-        elif i in [3, 6, 9]:
+            left = num
+        elif num in [3, 6, 9]:
             answer += 'R'
-            right = pad[str(i)]
+            right = num
         else:
-            left_dis = abs(pad[str(i)][0] - left[0]) + abs(pad[str(i)][1] - left[1])
-            right_dis = abs(pad[str(i)][0] - right[0]) + abs(pad[str(i)][1] - right[1])
-            if left_dis < right_dis:
-                answer += 'L'
-                left = pad[str(i)]
-            elif left_dis > right_dis:
+            left_dis = abs(pad[left][0] - pad[num][0]) + abs(pad[left][1] - pad[num][1])
+            right_dis = abs(pad[right][0] - pad[num][0]) + abs(pad[right][1] - pad[num][1])
+            
+            if left_dis > right_dis:
                 answer += 'R'
-                right = pad[str(i)]
+                right = num
+            elif left_dis < right_dis:
+                answer += 'L'
+                left = num
             else:
                 if hand == 'left':
                     answer += 'L'
-                    left = pad[str(i)]
+                    left = num
                 else:
                     answer += 'R'
-                    right = pad[str(i)]
+                    right = num
     return answer
