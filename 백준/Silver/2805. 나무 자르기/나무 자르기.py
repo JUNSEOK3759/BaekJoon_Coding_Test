86fc65991eeeb91689c3ef49abb337e14432f6f4
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-08-17 00:20:36 +0900

    [Silver II] Title: 나무 자르기, Time: 2120 ms, Memory: 150704 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,28 +1,25 @@
-import sys
-# sys.stdin = open("input.txt", "rt")
-
-input = sys.stdin.readline
-# readline을 문자열에서 쓸 때는 rstrip()를 붙여줘라
-sys.setrecursionlimit(10**9)
+def cut(mid, a):
+    sum = 0
+    for i in a:
+        if i > mid:
+            sum += i - mid
+    return sum
 
 n, m = map(int, input().split())
 
 a = list(map(int, input().split()))
+res = 0
 
 lt = 0
 rt = max(a)
-res = -2147483647
+
 while lt <= rt:
     mid = (lt + rt) // 2
-    cnt = 0
-    for i in a:
-        if i > mid:
-            cnt += (i - mid)
-    if cnt >= m:
-        res = mid
+    
+    if cut(mid, a) >= m:
         lt = mid + 1
+        res = mid
     else:
-        rt = mid -1
+        rt = mid - 1
 
 print(res)
-    
