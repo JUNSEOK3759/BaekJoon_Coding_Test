c3c4b271a31b60bf674a716cb12f525420131229
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-08-16 23:56:47 +0900

    [Silver IV] Title: 수 찾기, Time: 636 ms, Memory: 46736 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,31 +1,21 @@
-import sys
-# sys.stdin = open("input.txt", "rt")
-
-input = sys.stdin.readline
-# readline을 문자열에서 쓸 때는 rstrip()를 붙여줘라
-sys.setrecursionlimit(10**9)
-
 n = int(input())
-
-a = list(map(int, input().split()))
-a.sort()
-
-m = int(input())
-
 b = list(map(int, input().split()))
+b.sort()
+m = int(input())
+a = list(map(int, input().split()))
 
-for i in range(len(b)):
-    lt = 0      
-    rt = n-1            
+for i in a:
+    lt = 0
+    rt = n-1
+    
     while lt <= rt:
         mid = (lt + rt) // 2
-        if a[mid] == b[i]:
+        if b[mid] == i:
             print(1)
             break
+        elif b[mid] > i:
+            rt = mid -1
         else:
-            if b[i] > a[mid]:
-                lt = mid + 1
-            else:
-                rt = mid -1
+            lt = mid + 1
     else:
         print(0)
