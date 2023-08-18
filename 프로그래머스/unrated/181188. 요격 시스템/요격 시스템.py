901c81cdb978cca05ae1797c8d9910b90a897734
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-08-19 00:17:49 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,7 +1,8 @@
 def solution(targets):
     answer = 0
     targets.sort(key = lambda x : (x[1], x[0]))
-    e = 0
+    s, e = 0, 0
+    
     for i in targets:
         if i[0] >= e:
             answer += 1
