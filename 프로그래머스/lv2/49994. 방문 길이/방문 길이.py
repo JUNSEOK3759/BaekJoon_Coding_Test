0bae76495497405838b1a746fa40337267404310
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-08-18 18:35:34 +0900

    [level 2] Title: 방문 길이, Time: 0.60 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
@@ -1,13 +1,15 @@
 def solution(dirs):
-    answer = set()
-    move = {'U':[0, 1], 'D' : [0, -1], 'L' : [-1, 0], 'R': [1, 0]}
-    x = (0, 0)
+    move = {'U' : [0, 1], 'D' : [0, -1], 'L' : [-1, 0], 'R' : [1, 0]}
+    a = set()
+    point = (0, 0)
     for i in dirs:
-        a = x[0] + move[i][0]
-        b = x[1] + move[i][1]
-        if -6 < a < 6 and -6 < b < 6:
-            q = sorted([(x[0], x[1]), (a, b)])
-            answer.add(tuple(q))
-            x = (a, b)
-    return len(answer)
-
+        x = point
+        if  -5 <= point[0] + move[i][0] <= 5 and -5 <= point[1] + move[i][1] <= 5:
+            point = list(point)
+            point[0] += move[i][0]
+            point[1] += move[i][1]
+            point = tuple(point)
+            k = ((x, point), (point, x))
+            k = tuple(sorted(k))
+            a.add(k)
+    return len(a)
