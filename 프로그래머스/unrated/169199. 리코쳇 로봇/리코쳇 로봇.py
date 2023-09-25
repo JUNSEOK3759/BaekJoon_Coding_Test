e4003ab3aa01b838182d9f375f1f3732ffe921f1
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-09-25 22:49:49 +0900

    [unrated] Title: 리코쳇 로봇, Time: 2.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,37 +1,31 @@
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 from collections import deque
+def bfs(i, j, d, board, n, m, ch):
+    dq = deque()
+    dq.append([i, j, d])
+    while dq:
+        x, y, d = dq.popleft()
+        if board[x][y] == 'G':
+            return d
+        for i in range(4):
+            nx = x
+            ny = y 
+            while -1 < nx + dx[i] < n and -1 < ny + dy[i] < m and board[nx + dx[i]][ny + dy[i]] != 'D':
+                nx += dx[i]
+                ny += dy[i]
+            if ch[nx][ny] > d+1:
+                ch[nx][ny] = d+1
+                dq.append([nx, ny, d+1])
+    return -1
+
 def solution(board):
-    
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    def bfs(i, j):
-        dq = deque()
-        dq.append([i, j])
-        
-        while dq:
-            x, y = dq.popleft()
-            
-            if board[x][y] == 'G':
-                return ch[x][y]
-            for i in range(4):
-                nx = x
-                ny = y
-                
-                while -1< nx + dx[i] < n and -1 < ny + dy[i] < m and board[nx+dx[i]][ny + dy[i]] != 'D':
-                    nx += dx[i]
-                    ny += dy[i]
-                if ch[nx][ny] > ch[x][y] +1:
-                    ch[nx][ny] = ch[x][y] + 1
-                    dq.append([nx, ny])
-        return -1
-        
-    
+    answer = 0
     n = len(board)
     m = len(board[0])
     ch = [[2147483647 for _ in range(m)] for _ in range(n)]
-    cnt = 0
     for i in range(n):
         for j in range(m):
             if board[i][j] == 'R':
                 ch[i][j] = 0
-                cnt = bfs(i, j)
-    return cnt
+                return bfs(i, j, 0, board, n, m, ch)
