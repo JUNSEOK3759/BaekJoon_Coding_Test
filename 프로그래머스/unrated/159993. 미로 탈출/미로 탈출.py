39f03d30240f201b5d1a44502881975fe0429ed5
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-09-25 23:09:37 +0900

    [unrated] Title: 미로 탈출, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -1,49 +1,53 @@
 from collections import deque
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def sbfs(i, j, maps, n, m):
+    dq = deque()
+    dq.append([i, j, 0])
+    ch = [[0 for _ in range(m)] for _ in range(n)]
+    ch[i][j] = 1
+    while dq:
+        x, y, d = dq.popleft()
+        if maps[x][y] == 'L':
+            return d
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if -1 < nx < n and -1 < ny < m and ch[nx][ny] == 0 and maps[nx][ny] != 'X':
+                ch[nx][ny] = 1
+                dq.append([nx, ny, d+1])
+    return -1
+
+def ebfs(i, j, maps, n, m):
+    dq = deque()
+    dq.append([i, j, 0])
+    ch2 = [[0 for _ in range(m)] for _ in range(n)]
+    ch2[i][j] = 1
+    while dq:
+        x, y, d = dq.popleft()
+        if maps[x][y] == 'E':
+            return d
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if -1 < nx < n and -1 < ny < m and ch2[nx][ny] == 0 and maps[nx][ny] != 'X':
+                ch2[nx][ny] = 1
+                dq.append([nx, ny, d+1])
+    return -1
+
+                
 def solution(maps):
     answer = 0
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    len_x, len_y = len(maps), len(maps[0])
-    
-    
-    def lbfs(x, y):
-        dq = deque()
-        dq.append([x, y])
-        ch = [[0 for _ in range(len_y)] for _ in range(len_x)]
-        while dq:
-            x, y = dq.popleft()
-            
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                
-                if -1 < nx < len_x and -1 < ny < len_y and ch[nx][ny] == 0:
-                    if maps[nx][ny] == 'O' or maps[nx][ny] == 'E':
-                        ch[nx][ny] = ch[x][y] + 1
-                        dq.append([nx, ny])
-                    elif maps[nx][ny] == 'L':
-                        return ch[x][y] + 1
-                
-    def ebfs(x, y):
-        dq = deque()
-        dq.append([x, y])
-        ch2 = [[0 for _ in range(len_y)] for _ in range(len_x)]
-        while dq:
-            x, y = dq.popleft()
-            
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if -1 < nx < len_x and -1 < ny < len_y and ch2[nx][ny] == 0:
-                    if maps[nx][ny] == 'O' or maps[nx][ny] == 'S':
-                        ch2[nx][ny] = ch2[x][y] + 1
-                        dq.append([nx, ny])
-                    elif maps[nx][ny] == 'E':
-                        return ch2[x][y] + 1
-    for i in range(len_x):
-        for j in range(len_y):
+    n, m = len(maps), len(maps[0])
+    s, l = 0, 0
+    for i in range(n):
+        for j in range(m):
             if maps[i][j] == 'S':
-                L = lbfs(i,j)
-            if maps[i][j] == 'L':
-                E = ebfs(i, j)
-    return L + E if L and E else -1
+                s = sbfs(i, j, maps, n, m)
+            elif maps[i][j] == 'L':
+                l= ebfs(i, j, maps, n, m)
+                
+    return s + l if s != -1 and l != -1 else -1
