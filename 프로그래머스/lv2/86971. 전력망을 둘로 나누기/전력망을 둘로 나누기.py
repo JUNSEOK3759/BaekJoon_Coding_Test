252ab41e6b95f8efcf96a0b835a259c7f8158e99
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-09-27 20:34:45 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.54 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,36 +1,34 @@
 from collections import deque
+def bfs(k, ch, wires, n):
+    dq = deque()
+    dq.append(k)
+    visit = [0 for _ in range(n+1)]
+    visit[k] = 1
+    cnt = 1
+    while dq:
+        x = dq.popleft()
+        for i in ch[x]:
+            if visit[i] == 0:
+                visit[i] = 1
+                dq.append(i)
+                cnt += 1
+    return cnt
+
 def solution(n, wires):
     answer = 2147483647
-    
-    a = [[] for _ in range(n+1)]
+    ch = [[] for _ in range(n+1)]
     
     for x, y in wires:
-        a[x].append(y)
-        a[y].append(x)
+        ch[x].append(y)
+        ch[y].append(x)
     
-    def bfs(k):
-        dq = deque()
-        dq.append(k)
-        cnt = 1
-        ch = [0 for _ in range(n+1)]
-        ch[k] = 1
-        
-        while dq:
-            x = dq.popleft()
-            
-            for i in a[x]:
-                if ch[i] == 0:
-                    ch[i] = 1
-                    dq.append(i)
-                    cnt += 1
-        return cnt
     for x, y in wires:
-        a[x].remove(y)
-        a[y].remove(x)
+        ch[x].remove(y)
+        ch[y].remove(x)
         
-        answer = min(abs(bfs(x) - bfs(y)), answer)
+        answer = min(abs(bfs(x, ch, wires, n) - bfs(y, ch, wires, n)), answer)
         
-        a[x].append(y)
-        a[y].append(x)
-        
+        ch[x].append(y)
+        ch[y].append(x)
+    
     return answer
