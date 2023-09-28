a0c54455e8caf62794078e4848ac1ac0a4d97dba
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-09-28 16:22:18 +0900

    [level 2] Title: 숫자의 표현, Time: 6.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,19 +1,18 @@
 def solution(n):
-    answer = 0
+    answer = 1
     a = [i for i in range(1, n+1)]
     lt = 0
-    rt = 0
-    
-    while True:
-        x = sum(a[lt : rt+1])
+    rt = 1
+    while lt <= rt:
+        if lt == len(a)-1:
+            break
+        x = sum(a[lt:rt])
         if x == n:
-            lt += 1
-            rt = lt
             answer += 1
+            lt += 1
         elif x > n:
             lt += 1
         else:
             rt += 1
-        if lt == len(a):
-            break
+        
     return answer
