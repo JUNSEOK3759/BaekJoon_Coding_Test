df6d0ce7c5f07b75214ce0b5817684d6cbb001eb
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-09-28 15:25:39 +0900

    [unrated] Title: 과제 진행하기, Time: 1.76 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/unrated/176962. 과제 진행하기/과제 진행하기.py
@@ -1,30 +1,28 @@
 from collections import deque
+def makeTimeInt(plans):
+    for i in range(len(plans)):
+        x, y, z = plans[i]
+        y1, y2 = y.split(":")
+        start = (int(y1) * 60) + int(y2)
+        end = start + int(z)
+        plans[i] = [x, start, end]
+    return
+
 def solution(plans):
     answer = []
-    
-    def makeNewList(plans):
-        for i in range(len(plans)):
-            x, y, z = plans[i]
-            a, b = y.split(':')
-            y = int(a) * 60 + int(b)
-            z = int(z)
-            plans[i] = [x, y, y+z]
-        
-    
-    makeNewList(plans)
-    plans.sort(key = lambda x : x[1])
-    a = deque(plans)
+    makeTimeInt(plans)
+    plans.sort(key = lambda x : (x[1], x[2]))
+    p = deque(plans)
     stack = []
     
-    while a:
-        if len(a) >= 2 and a[0][2] > a[1][1]:
-            x, y, z = a.popleft()
-            stack.append([x, z - a[0][1]])
+    while p:
+        if len(p) >= 2 and p[0][2] > p[1][1]:
+            x, y, z = p.popleft()
+            stack.append([x, z - p[0][1]])
         else:
-            x, y, z = a.popleft()
+            x, y, z = p.popleft()
             answer.append(x)
-            
             if stack:
                 i, j = stack.pop()
-                a.appendleft([i, z, z+j])
+                p.appendleft([i, z, z + j])
     return answer
