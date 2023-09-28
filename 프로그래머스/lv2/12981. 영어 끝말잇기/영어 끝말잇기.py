34b2701d64189ffb6ee07cf19fd95cf8e9d43093
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-09-28 17:03:39 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,11 +1,9 @@
 def solution(n, words):
     answer = [words[0]]
-    
+
     for i in range(1, len(words)):
-        if words[i-1][-1] != words[i][0] or words[i] in answer:
-            return [i % n + 1, i // n + 1]
+        if words[i] in answer or answer[-1][-1] != words[i][0]:
+            return [i % n + 1 , i // n + 1]
         else:
             answer.append(words[i])
-    
-    
     return [0, 0]
