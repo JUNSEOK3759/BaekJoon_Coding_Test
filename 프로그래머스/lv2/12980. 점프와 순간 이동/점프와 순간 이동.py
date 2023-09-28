d24893d51303b8a36b6432bc91a0310a70b87c6b
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-09-28 17:07:26 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,12 +1,9 @@
-from collections import deque
 def solution(n):
     ans = 0
-    
-    while n > 0:
-        if n % 2 == 0:
-            n = n // 2
-        else:
+    while n:
+        if n % 2 == 1:
             ans += 1
             n -= 1
-    
+        else:
+            n /= 2
     return ans
