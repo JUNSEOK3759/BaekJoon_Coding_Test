bcef05de5bdf19b0d1cc265cef443349ac0b88fd
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-10-04 00:21:23 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,7 +1,6 @@
 -- 코드를 입력하세요
 SELECT a.TITLE, a.BOARD_ID, b.REPLY_ID, b.WRITER_ID, b.CONTENTS, date_format(b.CREATED_DATE, '%Y-%m-%d') as CREATED_DATE
-from USED_GOODS_BOARD a
-inner join USED_GOODS_REPLY b
+from USED_GOODS_BOARD a join USED_GOODS_REPLY b
 on a.BOARD_ID = b.BOARD_ID
-where a.created_date like '%2022-10%'
+where date_format(a.CREATED_DATE, '%Y-%m') = '2022-10'
 order by CREATED_DATE, a.title
