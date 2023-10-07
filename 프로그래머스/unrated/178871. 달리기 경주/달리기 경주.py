3bf10f5f23784ebb90b40def4869958cc4371fec
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-10-07 12:59:04 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,15 +1,15 @@
 def solution(players, callings):
     answer = []
-    player = {players[i] : i for i in range(len(players))}
-    rank = {i : players[i]  for i in range(len(players))}
+    a = {players[i] : i for i in range(len(players))}
+    b = {i : players[i] for i in range(len(players))}
     for i in callings:
-        x = player[i]
-        player[i] = x-1
-        player[rank[x-1]] = x
-         
-        rank[x] = rank[x-1]
-        rank[x-1] = i
+        x = a[i]
+        a[b[x]] = x-1
+        a[b[x-1]] = x
+        
+        b[x] = b[x-1]
+        b[x-1] = i
     
-    for i in rank.values():
-        answer.append(i)
+    for x in b.values():
+        answer.append(x)
     return answer
