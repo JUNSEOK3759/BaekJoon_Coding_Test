a29f86003197587a3137c8b8dc30382671c4ae88
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-11-24 00:50:02 +0900

    [level 2] Title: 롤케이크 자르기, Time: 903.46 ms, Memory: 51.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,14 @@
+from collections import Counter
+def solution(topping):
+    answer = 0
+    a = Counter(topping)
+    x = set()
+    
+    for i in topping:
+        a[i] -= 1
+        x.add(i)
+        if a[i] == 0:
+            a.pop(i)
+        if len(a) == len(x):
+            answer += 1
+    return answer
