13458bd025e5bd7bfbb94876c3c8d2c91654ff43
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-11-24 00:30:04 +0900

    [level 2] Title: 구명보트, Time: 10.90 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -3,14 +3,13 @@ def solution(people, limit):
     answer = 0
     people.sort()
     a = deque(people)
-    
     while len(a) >= 2:
-        if a[0] + a[-1] <= limit:
-                a.pop()
-                a.popleft()
-        else:
-            a.pop()
         answer += 1
+        if a[0] + a[-1] <= limit:
+            a.popleft()
+            a.pop()
+        else:
+            a.pop()  
     if a:
         answer += 1
     return answer
