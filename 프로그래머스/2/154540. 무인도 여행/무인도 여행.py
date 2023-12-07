2ba5c4a6fd6115e58ec75215bd391a3a67177673
JUNSEOK <76153178+JUNSEOK3759@users.noreply.github.com>
2023-12-08 00:47:51 +0900

    [level 2] Title: 무인도 여행, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,34 +1,34 @@
 from collections import deque
-
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-def bfs(x, y, len_x, len_y, maps, ch):
+def bfs(i, j, ch, len_x, len_y, maps):
     dq = deque()
-    d = int(maps[x][y])
-    dq.append([x, y])
+    ch[i][j] = 1
+    d = int(maps[i][j])
+    dq.append([i, j])
+    
     while dq:
         x, y = dq.popleft()
-        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if -1 < nx < len_x and -1 < ny < len_y and ch[nx][ny] == 0 and maps[nx][ny].isdigit():
+            if -1 < nx < len_x and -1 < ny < len_y and maps[nx][ny].isdigit() and ch[nx][ny] == 0:
                 ch[nx][ny] = 1
                 d += int(maps[nx][ny])
-                dq.append([nx, ny])
+                dq.append([nx,ny])
+                         
     return d
-    
 
 def solution(maps):
     answer = []
     len_x = len(maps)
     len_y = len(maps[0])
     ch = [[0 for _ in range(len_y)] for _ in range(len_x)]
-    
     for i in range(len_x):
         for j in range(len_y):
             if maps[i][j].isdigit() and ch[i][j] == 0:
-                ch[i][j] = 1
-                answer.append(bfs(i, j, len_x, len_y, maps, ch))
+                answer.append(bfs(i, j, ch, len_x, len_y, maps))
+
+    
     return sorted(answer) if answer else [-1]
